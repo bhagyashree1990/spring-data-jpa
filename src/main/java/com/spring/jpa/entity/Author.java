63fb27420a8d4f18579565c0b7f66f5dfba60d6d
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-08-03 16:22:26 +0530

    6 Association Bidirectional ManyToMany

--- src/main/java/com/spring/jpa/entity/Author.java
+++ src/main/java/com/spring/jpa/entity/Author.java
@@ -1,9 +1,13 @@
 package com.spring.jpa.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -15,6 +19,9 @@ public class Author {
 	
 	private String lastName;
 
+	@ManyToMany(mappedBy = "authors")
+	private List<Book> books = new ArrayList<>();
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -63,6 +70,14 @@ public class Author {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public List<Book> getBooks() {
+		return books;
+	}
+	
+	public void setBooks(List<Book> books) {
+		this.books = books;
 	}
 
 	@Override
---
+++ src/main/java/com/spring/jpa/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.spring.jpa.repository;
+
+import com.spring.jpa.entity.Author;
+
+public interface AuthorRepository extends BaseRepository<Author,Long> {
+}
