8c4d85f66e4d3cf62f3c088e22f15a86dd899a4a
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-08-03 15:48:38 +0530

    2 Assoication Unidirectional OneToOne
    Book
    Manuscript

---
+++ src/main/java/com/spring/jpa/entity/Manuscript.java
@@ -0,0 +1,78 @@
+package com.spring.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Manuscript {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String file;
+	
+	@OneToOne
+	@JoinColumn(name="book_id",foreignKey = @ForeignKey(name="book_id_fk"))
+	private Book book;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manuscript other = (Manuscript) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	@Override
+	public String toString() {
+		return "Manuscript [id=" + id + ", file=" + file + ", book=" + book + "]";
+	}
+		
+}
---
+++ src/main/java/com/spring/jpa/repository/ManuscriptRepository.java
@@ -0,0 +1,7 @@
+package com.spring.jpa.repository;
+
+import com.spring.jpa.entity.Manuscript;
+
+public interface ManuscriptRepository extends BaseRepository<Manuscript, Long>{
+
+}
