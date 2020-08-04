936f5ee7c0d494ce41e7e1e4ae7aad255cd4d90b
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-08-04 13:14:33 +0530

    12 Using @Query Annotation
    1. JPQL Query
    2. ORDER BY in JPQL
    3. Using #entityName
    4. Sort, Pageable as method parameter
    5. LIKE in JPQL
    6. Native Query
    7. Using @Modifying Query
    8. Using LocalDate as URL query parameter

--- src/main/java/com/spring/jpa/repository/AuthorRepository.java
+++ src/main/java/com/spring/jpa/repository/AuthorRepository.java
@@ -1,6 +1,63 @@
 package com.spring.jpa.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import com.spring.jpa.entity.Author;
 
 public interface AuthorRepository extends BaseRepository<Author,Long> {
+	/**
+	 * ORDER BY Clause in JPQL Query
+	 * @param firstName
+	 * @return
+	 */
+	@Query("FROM Author where firstName = ?1 ORDER BY lastName desc")
+	List<Author> findByFirstNameOrderByLastName(@Param("firstName") String firstName);
+	
+	/**
+	 * Add Sort parameter in repository method
+	 * Using expression to set Entity Name
+	 * @param firstName
+	 * @param sort
+	 * @return
+	 */
+	@Query("FROM #{#entityName} where firstName = ?1")
+	List<Author> findByFirstName(@Param("firstName") String firstName, Sort sort);
+	
+	/**
+	 * LIKE in JPQL Query
+	 * @param firstName
+	 * @return
+	 */
+	@Query("FROM #{#entityName} where UPPER(firstName) LIKE %?#{[0].toUpperCase()}%")
+	List<Author> findByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
+	
+	/**
+	 * Native Query
+	 * @param lastName
+	 * @return
+	 */
+	@Query(value="select * from author where last_name = :lastName",nativeQuery = true)
+	List<Author> findByLastName(@Param("lastName") String lastName);
+	
+	/**
+	 * Modifying Query
+	 * @param dateOfBirth
+	 * @param id
+	 */
+	@Query("UPDATE Author SET dateOfBirth = :dateOfBirth where id = :id")
+	@Modifying
+	@Transactional
+	Integer updateDateOfBirth(
+			@DateTimeFormat(iso = ISO.DATE)
+			@Param("dateOfBirth")LocalDate dateOfBirth,@Param("id")Long id);
 }
